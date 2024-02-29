1560f3840aad09f402e8db05a0cc02774ff3657e
JulioXC4 <danielc4nt@gmail.com>
2024-02-29 00:08:45 -0500

    improved the responsive section of the landing page

--- front-end/src/app/components/LandingPage.tsx
+++ front-end/src/app/components/LandingPage.tsx
@@ -7,7 +7,7 @@ const LandingPage = () => {
     <div className="w-full h-[100vh] bg-hero_primary overflow-hidden relative">
       <img
         src={"/images/dog.webp"}
-        className="absolute bottom-0 right-0 z-10 h-3/4"
+        className="hidden lg:block absolute bottom-0 right-0 z-10 h-3/4"
         alt="Dog"
       />
       <div className="absolute top-1/4 left-1/3 transform -translate-x-1/2 -translate-y-1/2 bg-hero_ligth_secondary rounded-full w-48 h-48"></div>
@@ -15,8 +15,8 @@ const LandingPage = () => {
       <div className="absolute top-0 right-0 transform -translate-x-1/2 -translate-y-1/2 bg-hero_ligth_secondary rounded-full w-96 h-96"></div>
       <div className="absolute -bottom-1/4 right-20 bg-hero_ligth_secondary rounded-full w-96 h-96"></div>
       <div className="flex h-full w-full items-center justify-center ">
-        <div className="flex flex-col w-3/4 h-full justify-center items-end">
-          <h1 className="max-w-md text-sm md:text-lg lg:text-2xl xl:text-3xl font-semibold font-sans text-balance leading-tight z-50 m-6 transform translate-x-20 duration-500 ease-out">
+        <div className="flex flex-col w-full lg:w-3/4 h-full justify-center items-center lg:items-end">
+          <h1 className="max-w-md text-2xl md:text-lg lg:text-2xl xl:text-3xl font-semibold font-sans text-balance leading-tight z-50 m-6 transform translate-x-20 duration-500 ease-out">
             En <span className="text-identipet">Pet Hub</span>, llevamos la
             seguridad de tus{" "}
             <span className="text-identipet">peludos amigos</span> al
